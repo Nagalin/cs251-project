ba3ba3fb0e6d69f8cd44953138470a607cf4fcd7
Nagalin punyakalin <nagalin.pun@dome.tu.ac.th>
2023-05-06 17:50:15 +0700

    remove outlet

--- frontend/src/component/navbar/navbar.jsx
+++ frontend/src/component/navbar/navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 
 import style from "./navbar.module.css";
